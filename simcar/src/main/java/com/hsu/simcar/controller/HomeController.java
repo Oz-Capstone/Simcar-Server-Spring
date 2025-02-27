6fd6d47544cb3f7b1a27fe94ab7f3709b99d0a8a
eekfkt <168187962+eekfkt@users.noreply.github.com>
2025-02-28 03:04:24 +0900

    수정

--- simcar/src/main/java/com/hsu/simcar/controller/HomeController.java
+++ simcar/src/main/java/com/hsu/simcar/controller/HomeController.java
@@ -2,17 +2,14 @@ package com.hsu.simcar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-
-    @GetMapping(value = "/")
-    public String index() {
-        return "forward:/index.html";
-    }
-
-    @GetMapping(value = "/{path:^(?!api|swagger-ui|v3/api-docs|h2-console).*}/**")
-    public String forwardToIndex() {
-        return "forward:/index.html";
+    
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "서버가 정상적으로 동작 중입니다. 정적 파일이 올바르게 로드되지 않고 있습니다.";
     }
 }
