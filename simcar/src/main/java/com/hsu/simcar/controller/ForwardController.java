e4443ac40bcc238f47be0ef9e2ea5821469a50b2
eekfkt <168187962+eekfkt@users.noreply.github.com>
2025-02-27 15:28:12 +0900

    프로덕션빌드

---
+++ simcar/src/main/java/com/hsu/simcar/controller/ForwardController.java
@@ -0,0 +1,11 @@
+package com.hsu.simcar.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
